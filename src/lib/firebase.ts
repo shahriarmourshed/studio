eb2de4635a150adef4ddbb93a7b4518c5e991873
Shahriar Mourshed <shahriarmourshed@gmail.com>
2025-08-29 18:35:47 +0000

    Tool is no longer active

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  projectId: "familyverse-5xnxn",
-  appId: "1:314321317316:web:abb25ed31233fb4cb68891",
-  storageBucket: "familyverse-5xnxn.firebasestorage.app",
-  apiKey: "AIzaSyAOnRgf3OSlJ4F516dmtoylNZpVi1ofSLA",
-  authDomain: "familyverse-5xnxn.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "314321317316"
+  "projectId": "familyverse-5xnxn",
+  "appId": "1:314321317316:web:4077473d0083408bb68891",
+  "storageBucket": "familyverse-5xnxn.firebasestorage.app",
+  "apiKey": "AIzaSyAOnRgf3OSlJ4F516dmtoylNZpVi1ofSLA",
+  "authDomain": "familyverse-5xnxn.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "314321317316"
 };
 
 // Initialize Firebase
