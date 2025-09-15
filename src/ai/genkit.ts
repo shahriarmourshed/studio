f4752abcfb595d4bf9999c10e1ee27f821ca44a7
Shahriar Mourshed <shahriarmourshed@gmail.com>
2025-09-15 12:51:29 +0000

    Try fixing this error: `Build Error: Export firebase doesn't exist in ta

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,7 @@
 
 import {genkit, GenkitError} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import {firebase} from '@genkit-ai/firebase';
+import {firebase} from '@genkit-ai/firebase/plugin';
 import {runWith} from 'genkit/context';
 import {getFirestore} from 'firebase-admin/firestore';
 
